77ea01b1f47aaeccdd5c04560fc95873eba7f526
safwankdb <kdbeatbox@gmail.com>
2018-07-06 16:56:18 +0530

    Prints score

--- LinReg.py
+++ LinReg.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 import pickle
 
 
-# In[55]:
+# In[68]:
 
 
 data = np.array(pd.read_csv('data.csv'))
@@ -21,21 +21,21 @@ X = data[:,:-1]
 y = data[:,-1]
 
 
-# In[56]:
+# In[69]:
 
 
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.3)
 
 
-# In[57]:
+# In[70]:
 
 
 regr = linear_model.LinearRegression()
 regr.fit(X_train,y_train)
-y_pred=regr.predict(X_test)
+y_pred = regr.predict(X_test)
 
 
-# In[58]:
+# In[71]:
 
 
 plt.scatter(X_test[:,1],y_test,label='Original Data')
@@ -46,7 +46,7 @@ plt.ylabel('Output')
 plt.savefig('Plot 1.png')
 
 
-# In[59]:
+# In[72]:
 
 
 plt.scatter(X_test[:,2],y_test,label='Original Data')
@@ -57,7 +57,7 @@ plt.ylabel('Output')
 plt.savefig('Plot 2.png')
 
 
-# In[60]:
+# In[73]:
 
 
 plt.scatter(X_test[:,3],y_test,label='Original Data')
@@ -68,8 +68,20 @@ plt.ylabel('Output')
 plt.savefig('Plot 3.png')
 
 
-# In[61]:
+# In[74]:
 
 
 pickle.dump(regr,open('Linear Regression Model','wb'))
+
+
+# In[75]:
+
+
+print('r2 Score for model is',r2_score(y_test,y_pred))
+
+
+# In[63]:
+
+
+print('Mean squared error is',mean_squared_error(y_test,y_pred))
 
